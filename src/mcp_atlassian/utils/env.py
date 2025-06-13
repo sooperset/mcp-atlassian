36945f4379bd261796df8331454c4591046e304e
Dennis Paul <dennis@paul.hamburg>
2025-06-13 15:38:16 +0200

    remove is_env_falsy

--- src/mcp_atlassian/utils/env.py
+++ src/mcp_atlassian/utils/env.py
@@ -16,18 +16,3 @@ def is_env_truthy(env_var_name: str, default: str = "") -> bool:
         True if the environment variable is set to a truthy value, False otherwise
     """
     return os.getenv(env_var_name, default).lower() in ("true", "1", "yes", "y", "on")
-
-
-def is_env_falsy(env_var_name: str, default: str = "") -> bool:
-    """Check if an environment variable is set to a falsy value.
-
-    Considers 'false', '0', 'no', 'n', 'off' as falsy values (case-insensitive).
-
-    Args:
-        env_var_name: Name of the environment variable to check
-        default: Default value if environment variable is not set
-
-    Returns:
-        True if the environment variable is set to a falsy value, False otherwise
-    """
-    return os.getenv(env_var_name, default).lower() in ("false", "0", "no", "n", "off")
